fc620b9c1a8b5699830bc4476910127014dbaca6
Lucas da Luz <daluzdev@gmail.com>
2022-06-23 14:45:56 -0300

    Add https redirection in startup

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -36,10 +36,14 @@ namespace Api
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Api v1"));
+                app.UseSwaggerUI(c => 
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Api v1");
+                    c.RoutePrefix = "";
+                });
             }
 
-            //app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
 
             app.UseRouting();
 
